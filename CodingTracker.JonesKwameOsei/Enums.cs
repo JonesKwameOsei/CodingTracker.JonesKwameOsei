72386663496a1f9aa915b26367f4b0a98026c301
JonesKwameOsei <oseikwamejones@gmail.com>
2025-08-29 06:27:53 +0100

    Add new options to MainMenuOptions enum
    Added `StartLiveCodingSession` and `ViewCodingSessions`
    to the `MainMenuOptions` internal enum in `Enums.cs`,
    including `Display` attributes for improved user interface.

--- CodingTracker.JonesKwameOsei/Enums.cs
+++ CodingTracker.JonesKwameOsei/Enums.cs
@@ -9,6 +9,9 @@ internal class Enums
         [Display(Name = "Add Coding Session")]
         AddCodingSession,
 
+        [Display(Name = "Start Live Coding Session")]
+        StartLiveCodingSession,
+
         [Display(Name = "View Coding Sessions")]
         ViewCodingSessions,
 
