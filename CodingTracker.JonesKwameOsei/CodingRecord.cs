fcb2c786d0f86087b2e0d1cd3a6a9979c4442d43
JonesKwameOsei <oseikwamejones@gmail.com>
2025-08-29 04:29:55 +0100

    Update Language property in CodingRecord class
    Changed Language from nullable to non-nullable string and set a default value of "C#".

--- CodingTracker.JonesKwameOsei/CodingRecord.cs
+++ CodingTracker.JonesKwameOsei/CodingRecord.cs
@@ -3,7 +3,7 @@
 internal class CodingRecord
 {
     internal int Id { get; set; }
-    internal string? Language { get; set; }
+    internal string Language { get; set; } = "C#";
     internal DateTime DateStart { get; set; }
     internal DateTime DateEnd { get; set; }
     internal TimeSpan Duration { get; set; }
